99d8af0c15f62356ad45270b19eb811deeed741e
Rommel Rico <rommeltj@gmail.com>
2017-11-30 21:24:20 -0800

    Updated gitignore and config

--- purnkleen/settings/production.py
+++ purnkleen/settings/production.py
@@ -17,7 +17,7 @@ from configparser import RawConfigParser
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 config = RawConfigParser()
-config.read(os.path.join(BASE_DIR, 'config', 'settings.ini'))
+config.read(os.path.join(BASE_DIR, 'config', 'settings_production.ini'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
