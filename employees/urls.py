05ad574126881617dd24c58f7c2cc8f5c81fdcca
Rommel Rico <rommeltj@gmail.com>
2017-12-08 22:24:36 -0800

    The Employee Update View

--- employees/urls.py
+++ employees/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 
 from .views import (
     EmployeeCreateView,
+    EmployeeUpdateView,
 )
 
 app_name = 'employees'
 urlpatterns = [
     url(r'^create/$', EmployeeCreateView.as_view(), name='create'),
+    url(r'^(?P<pk>\d+)/edit/$', EmployeeUpdateView.as_view(), name='update'),
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from django.urls import reverse_lazy
 
 from .forms import EmployeeForm
+from .mixins import UserOwnerMixin
 from .models import Employee, generate_next_emp_no
 
 
@@ -49,3 +50,31 @@ class EmployeeCreateView(LoginRequiredMixin, CreateView):
             type__in=["MEM","AFF"]
         ).first()
         return qs
+
+
+class EmployeeUpdateView(LoginRequiredMixin, UserOwnerMixin, UpdateView):
+    form_class = EmployeeForm
+    template_name = 'employees/employee_update.html'
+    model = Employee
+    success_url = reverse_lazy('index')
+
+    def get_initial(self):
+        initial = super(EmployeeUpdateView, self).get_initial()
+        initial['first_name'] = self.request.user.first_name
+        initial['last_name'] = self.request.user.last_name
+        return initial
+
+    def post(self, request, *args, **kwargs):
+        form = EmployeeForm(request.POST, request.FILES)
+        if form.is_valid():
+            self.object = form.save(commit=False)
+            self.object.emp_no = kwargs['pk']
+            self.object.user = self.request.user
+            self.object.user.first_name = form.data['first_name']
+            self.object.user.last_name = form.data['last_name']
+            self.object.user.save()
+            self.object.save()
+            return HttpResponseRedirect('/')
+        else:
+            print("form is invalid")
+        return render(request, self.template_name, {'form': form})
