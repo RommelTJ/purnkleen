488e1a2fc4e7f3a68e5461a615d764e5852acdf2
Rommel Rico <rommeltj@gmail.com>
2017-12-09 09:54:10 -0800

    Non-field error in UserOwnerMixin

--- employees/mixins.py
+++ employees/mixins.py
@@ -1,7 +1,8 @@
 class UserOwnerMixin(object):
+
     def form_valid(self, form):
         if form.instance.user == self.request.user:
             return super(UserOwnerMixin, self).form_valid(form)
         else:
-            form.add_error("content", "This user is not allowed to change this data.")
+            form.add_error(None, "You are not allowed to edit this user.")
             return self.form_invalid(form)
