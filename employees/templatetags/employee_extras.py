3304183767810612850e923408ac9741ab82f935
Rommel Rico <rommeltj@gmail.com>
2017-12-14 22:13:38 -0800

    Filter for concatenating strings

--- employees/templatetags/employee_extras.py
+++ employees/templatetags/employee_extras.py
@@ -6,3 +6,9 @@ register = template.Library()
 @register.filter
 def get_value(d, key):
     return d.get(key)
+
+
+@register.filter
+def addstr(arg1, arg2):
+    """concatenate arg1 & arg2"""
+    return str(arg1) + str(arg2)
