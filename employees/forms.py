b1e7d3d4ed27533cab37217800d78c19fa9957ee
Rommel Rico <rommeltj@gmail.com>
2017-12-16 09:08:26 -0800

    Adding an Image Field Widget to the Employee form

--- employees/forms.py
+++ employees/forms.py
@@ -1,7 +1,18 @@
 from django import forms
 from django_countries.widgets import CountrySelectWidget
+from django.utils.safestring import mark_safe
 
 from .models import Employee
+
+
+class EmployeeImageFieldWidget(forms.widgets.FileInput):
+    def __init__(self, placeholder='/media/profile/star_citizen_marine_400x400.jpg'):
+        self.placeholder = placeholder
+        super(EmployeeImageFieldWidget, self).__init__({})
+
+    def render(self, name, value, attrs=None, **kwargs):
+        image_html = '<img src="%s" />' % value.medium.url if value and hasattr(value, "url") else '<img src="%s" />' % self.placeholder
+        return mark_safe("%s%s" % (image_html, super(EmployeeImageFieldWidget, self).render(name, value, attrs)))
 
 
 class EmployeeForm(forms.ModelForm):
@@ -29,6 +40,12 @@ class EmployeeForm(forms.ModelForm):
     birth_date = forms.DateField(
         widget=forms.SelectDateWidget(years=range(1950, 2030)),
         help_text="Not required but used to wish you happy birthday."
+    )
+    image = forms.ImageField(
+        label="Profile Image",
+        widget=EmployeeImageFieldWidget(),
+        required=False,
+        help_text="Please upload a square 400x400 image."
     )
 
     def __str__(self):
