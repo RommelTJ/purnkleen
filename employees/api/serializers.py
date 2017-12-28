b0d69b1e23c931507ab8d3a0735dca670e4e58a7
Rommel Rico <rommeltj@gmail.com>
2017-12-27 19:08:31 -0800

    Adding a StdImageFieldSerializer

--- employees/api/serializers.py
+++ employees/api/serializers.py
@@ -4,27 +4,53 @@ from accounts.api.serializers import UserDisplaySerializer
 from employees.models import Employee
 
 
+class StdImageFieldSerializer(serializers.ImageField):
+    """
+    Get all the variations of the StdImageField
+    """
+
+    def to_native(self, obj):
+        return self.get_variations_urls(obj)
+
+    def to_representation(self, obj):
+        return self.get_variations_urls(obj)
+
+    def get_variations_urls(self, obj):
+        """
+        Get all the logo urls.
+        """
+
+        # Initiate return object
+        return_object = {}
+
+        # Get the field of the object
+        field = obj.field
+
+        # A lot of ifs going around, first check if it has the field variations
+        if hasattr(field, 'variations'):
+            # Get the variations
+            variations = field.variations
+            # Go through the variations dict
+            for key in variations.keys():
+                # Just to be sure if the stdimage object has it stored in the obj
+                if hasattr(obj, key):
+                    # get the by stdimage properties
+                    field_obj = getattr(obj, key, None)
+                    if field_obj and hasattr(field_obj, 'url'):
+                        # store it, with the name of the variation type into our return object
+                        return_object[key] = super(StdImageFieldSerializer, self).to_representation(field_obj)
+
+        # Also include the original (if possible)
+        if hasattr(obj, 'url'):
+            return_object['original'] = super(StdImageFieldSerializer, self).to_representation(obj)
+
+        return return_object
+
+
 class EmployeeModelSerializer(serializers.ModelSerializer):
     user = UserDisplaySerializer(read_only=True)
+    image = StdImageFieldSerializer()
 
     class Meta:
         model = Employee
         fields = "__all__"
-
-
-    # emp_no = models.IntegerField(primary_key=True)
-    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    # type = models.CharField(max_length=3, choices=EMPLOYEE_TYPES, default='AFF')
-    # callsign = models.CharField(max_length=255)
-    # rsi_url = models.URLField(default='https://robertsspaceindustries.com/citizens/croberts68')
-    # birth_date = models.DateField(default=datetime(1980, 1, 1))
-    # hire_date = models.DateTimeField(default=timezone.now, blank=True)
-    # country = CountryField(blank_label='(select country)')
-    # primary_activity = models.CharField(max_length=3, choices=ACTIVITY_CHOICES, default='RES')
-    # secondary_activity = models.CharField(max_length=3, choices=ACTIVITY_CHOICES, default='TRD')
-    # bio = models.TextField(max_length=1000, blank=True)
-    # image = StdImageField(upload_to=upload_path_handler, storage=fs, null=True, blank=True, max_length=255, variations={
-    #     'large': (400, 400),
-    #     'thumbnail': (100,100, True),
-    #     'medium': (200, 200),
-    # })
